a922d6fedde51f1a8a993843fe03b8237f493b78
isurojit <iamsurojit@hotmail.com>
2022-08-17 13:01:01 +0530

    Cart file structure added

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./CartButton.module.css";
+
+const CartButton = () => {
+  return <div>CartButton</div>;
+};
+
+export default CartButton;
---
+++ src/components/Cart/CartItems.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./CartItems.module.css";
+
+const CartItems = () => {
+  return <div>CartItems</div>;
+};
+
+export default CartItems;
