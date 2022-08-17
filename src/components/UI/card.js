731f9af7a251973c99250d918da67268b9889af4
isurojit <iamsurojit@hotmail.com>
2022-08-17 12:55:03 +0530

    file structure and card Ui added

---
+++ src/components/UI/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./card.module.css";
+
+const card = (props) => {
+  return (
+    <div
+      className={`${classes.card} ${props.className ? props.className : ""}`}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default card;
